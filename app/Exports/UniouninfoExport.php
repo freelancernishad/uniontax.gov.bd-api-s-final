749006e1d3570c9eb426c6a80786547697b73f17
Nishad Hossain <freelancernishad123@gmail.com>
2025-02-11 09:49:40 +0600

    update

--- app/Exports/UniouninfoExport.php
+++ app/Exports/UniouninfoExport.php
@@ -4,9 +4,12 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class UniouninfoExport implements FromCollection, WithHeadings
+class UniouninfoExport implements FromCollection, WithHeadings, ShouldAutoSize, WithColumnWidths, WithStyles
 {
     protected $data;
 
@@ -15,15 +18,52 @@ class UniouninfoExport implements FromCollection, WithHeadings
         $this->data = $data;
     }
 
+    /**
+     * Return the collection of data
+     */
     public function collection()
     {
-        return new Collection($this->data);
+        return collect($this->data);
     }
 
+    /**
+     * Define column headings
+     */
     public function headings(): array
     {
         return [
-            'Merchant ID', 'Pass', 'Organization', 'IP', 'Mobile', 'URL'
+            'Merchant ID', 
+            'Organization', 
+            'Server IP', 
+            'Mobile', 
+            'Password', 
+            'URL'
+        ];
+    }
+
+    /**
+     * Set column widths
+     */
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 25, // Merchant ID
+            'B' => 40, // Organization
+            'C' => 20, // Server IP
+            'D' => 15, // Mobile
+            'E' => 15, // Password
+            'F' => 40, // URL
+        ];
+    }
+
+    /**
+     * Style the spreadsheet
+     */
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true], 'fill' => ['fillType' => 'solid', 'startColor' => ['rgb' => 'FFFF00']]], // Yellow Header
+            'A1:F1' => ['alignment' => ['horizontal' => 'center']], // Center align headers
         ];
     }
 }
