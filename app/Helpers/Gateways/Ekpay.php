8c687fb419c180ce0bb14dd6209f6b34b6de8a77
Nishad Hossain <freelancernishad123@gmail.com>
2025-07-14 11:48:07 +0600

    update

--- app/Helpers/Gateways/Ekpay.php
+++ app/Helpers/Gateways/Ekpay.php
@@ -196,8 +196,9 @@ function sonodpayment($id, $urls, $hasEnData = false,$uddoktaId=null)
 
 
     if ($sonod->bokeya > 0) {
-        $vat = $sonod->bokeya * 0.15;
-        $total_amount = $sonod->bokeya + $vat;
+        // $vat = $sonod->bokeya * 0.15;
+        // $total_amount = $sonod->bokeya + $vat;
+        $total_amount = $sonod->bokeya;
     }
 
 
