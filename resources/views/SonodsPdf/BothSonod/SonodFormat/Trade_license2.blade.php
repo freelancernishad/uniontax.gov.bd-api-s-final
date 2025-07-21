673de0da3bd4371af66fa889b8a346b555bfcebc
Nishad Hossain <freelancernishad123@gmail.com>
2025-07-22 00:04:03 +0600

    update

--- resources/views/SonodsPdf/BothSonod/SonodFormat/Trade_license2.blade.php
+++ resources/views/SonodsPdf/BothSonod/SonodFormat/Trade_license2.blade.php
@@ -2,45 +2,22 @@
 $orthoBchor = explode('-',$rowbn->orthoBchor);
 Log::info('orthoBchor', ['orthoBchor' => $orthoBchor]);
 @endphp
-{{-- @php
-$orthoBchor = explode('-',$row->orthoBchor);
-@endphp
-
-<p style="margin-bottom: 10px;">৩০ জুন, {{ int_en_to_bn('20'.$orthoBchor[1]) }} তারিখ পর্যন্ত বৈধ ফি প্রদানের পরিমাণ  {{ int_en_to_bn($row->total_amount) }}  টাকা  কথায়: {{ $row->the_amount_of_money_in_words }}   প্রাপ্ত হয়ে তার ব্যবসা/বৃত্তি/পেশা চালিয়ে যাওয়ার জন্য এই লাইসেন্স প্রদান করা হলো।<br>
-</p> --}}
-
-{{-- <table width="100%" style="margin-bottom: 10px;font-size:12px;text-align:justify">
-    <tr>
-         <td width="50%" style="vertical-align: top;">
-            <p style="font-size: 14px;">স্থানীয় সরকার (ইউনিয়ন পরিষদ) আইন, ২০০৯ (২০০৯ সনের ৬১ নং আইন) এর ধারা ৬৬ তে প্রদত্ত ক্ষমতাবলে সরকার প্রণীত আদর্শ কর তফসিল, ২০১৩ এর ৬ ও ১৭ নং অনুচ্ছেদ অনুযায়ী ব্যবসা, বৃত্তি, পেশা, বা শিল্প প্রতিষ্ঠানের উপর আরোপিত কর আদায়ের লক্ষ্যে নির্ধারিত শর্তে নিম্নবর্ণিত ব্যক্তি/প্রতিষ্ঠানের অনুকূলে এই ট্রেড লাইসেন্সটি ইস্যু করা হলো: <br></p>
-        </td>
-        <td width="100%" style="vertical-align: top;">
-            <p style="font-size: 10px;">Under the authority granted by Section 66 of the Local Government (Union Parishad) Act, 2009 (Act No. 61 of 2009), and in accordance with Sections 6 and 17 of the Standard Tax Schedule, 2013, issued by the government for the purpose of collecting taxes imposed on businesses, professions, trades, or industrial establishments, this Trade License is issued to the following individual/establishment under the specified conditions: <br></p>
-        </td>
-
-        <td width="100%" style="vertical-align: top;">
-            <p style="font-size: 14px;">স্থানীয় সরকার (ইউনিয়ন পরিষদ) আইন, ২০০৯ (২০০৯ সনের ৬১ নং আইন) এর ধারা ৬৬ তে প্রদত্ত ক্ষমতাবলে সরকার প্রণীত আদর্শ কর তফসিল, ২০১৩ এর ৬ ও ১৭ নং অনুচ্ছেদ অনুযায়ী ব্যবসা, বৃত্তি, পেশা, বা শিল্প প্রতিষ্ঠানের উপর আরোপিত কর আদায়ের লক্ষ্যে নির্ধারিত শর্তে নিম্নবর্ণিত ব্যক্তি/প্রতিষ্ঠানের অনুকূলে এই ট্রেড লাইসেন্সটি ইস্যু করা হলো: <br></p>
-        </td>
-
-    </tr>
-</table> --}}
-
 
 
 
 
 @if(isUnion())
 
-<p style="font-size: 14px;text-align:justify">স্থানীয় সরকার (ইউনিয়ন পরিষদ) আইন, ২০০৯ (২০০৯ সনের ৬১ নং আইন) এর ধারা ৬৬ তে প্রদত্ত ক্ষমতাবলে সরকার প্রণীত আদর্শ কর তফসিল, ২০১৩ এর ৬ ও ১৭ নং অনুচ্ছেদ অনুযায়ী ব্যবসা, বৃত্তি, পেশা, বা শিল্প প্রতিষ্ঠানের উপর আরোপিত কর আদায়ের লক্ষ্যে নির্ধারিত শর্তে নিম্নবর্ণিত ব্যক্তি/প্রতিষ্ঠানের অনুকূলে এই ট্রেড লাইসেন্সটি ইস্যু করা হলো: </p>
+<p style="font-size: 13px;text-align:justify">স্থানীয় সরকার (ইউনিয়ন পরিষদ) আইন, ২০০৯ (২০০৯ সনের ৬১ নং আইন) এর ধারা ৬৬ তে প্রদত্ত ক্ষমতাবলে সরকার প্রণীত আদর্শ কর তফসিল, ২০১৩ এর ৬ ও ১৭ নং অনুচ্ছেদ অনুযায়ী ব্যবসা, বৃত্তি, পেশা, বা শিল্প প্রতিষ্ঠানের উপর আরোপিত কর আদায়ের লক্ষ্যে নির্ধারিত শর্তে নিম্নবর্ণিত ব্যক্তি/প্রতিষ্ঠানের অনুকূলে এই ট্রেড লাইসেন্সটি ইস্যু করা হলো: </p>
 
-<p style="font-size: 10px;text-align:justify">
+<p style="font-size: 9px;text-align:justify">
     Under the authority granted by Section 66 of the Local Government (Union Parishad) Act, 2009 (Act No. 61 of 2009), and in accordance with Sections 6 and 17 of the Standard Tax Schedule, 2013, issued by the government for the purpose of collecting taxes imposed on businesses, professions, trades, or industrial establishments, this Trade License is issued to the following individual/establishment under the specified conditions:
 </p>
 
 @else
-<p style="font-size: 14px;text-align:justify">এই লাইসেন্সটি নিম্নবর্ণিত ব্যক্তি/প্রতিষ্ঠানের জন্য ইস্যু করা হলো, যা স্থানীয় সরকার (পৌরসভা) আইন, ২০০৯ এর ধারা ৯৮ অনুসারে সরকার কর্তৃক পৌরসভাগুলির জন্য প্রণীত আদর্শ কর তফসিল ২০১৪ এর ৬ নম্বর সূচি অনুযায়ী আরোপিত কর সংগ্রহের উদ্দেশ্যে। </p>
+<p style="font-size: 13px;text-align:justify">এই লাইসেন্সটি নিম্নবর্ণিত ব্যক্তি/প্রতিষ্ঠানের জন্য ইস্যু করা হলো, যা স্থানীয় সরকার (পৌরসভা) আইন, ২০০৯ এর ধারা ৯৮ অনুসারে সরকার কর্তৃক পৌরসভাগুলির জন্য প্রণীত আদর্শ কর তফসিল ২০১৪ এর ৬ নম্বর সূচি অনুযায়ী আরোপিত কর সংগ্রহের উদ্দেশ্যে। </p>
 
-<p style="font-size: 10px;text-align:justify">
+<p style="font-size: 9px;text-align:justify">
     This license is issued to the undermentioned person/institution for the collection of imposed tax according to Schedule 6 of the Model Tax Schedule 2014, enacted for municipalities by the government under Section 98 of the Local Government (Municipality) Act 2009.
 </p>
 @endif
@@ -238,7 +215,7 @@ $orthoBchor = explode('-',$row->orthoBchor);
 
     <hr>
 
-    <table width='100%' style="font-size: 10px;">
+    <table width='100%' style="font-size: 10px;margin-top:5px">
         <tr>
             <td width='50%'>
 
